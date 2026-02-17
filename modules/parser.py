ccb30c8df58cac8748add0166f35742dc321baff
fayendambao067-rgb <fayendambao067@gmail.com>
2026-02-17 22:17:46 +0100

    Implémentation de parser.py

--- modules/parser.py
+++ modules/parser.py
@@ -1,8 +1,27 @@
 from bs4 import BeautifulSoup
 
 def parse_results(html_content):
-    """Fonction pour résumer le texte du site web"""
-    return {
-        "nom": "À déterminer",
-        "resume": "Résumé extrait de la page..."
-    }
+    """"Fonction pour résumer le texte du site web"""
+
+    if not html_content or "Erreur" in html_content:
+        return "Contenu indisponible ou erreur de lecture."
+
+    # on transforme le code brut en objet BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Retirer les éléments inutiles pour alléger
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.decompose()
+
+    #on récupère le texte des paragraphes <p>
+    paragraphs = soup.find_all('p')
+
+    # on rassemble le texte des 3 premiers paragraphes
+    text_list = [p.get_text().strip() for p in paragraphs if len(p.get_text()) > 20]
+    full_text = " ".join(text_list[:3])
+
+    # Si on ne trouve rien, on essaie de prendre le texte global
+    if not full_text:
+        full_text = soup.get_text(separator=' ', strip=True)[:300]
+
+    return full_text if full_text else "Aucun texte descriptif trouvé sur la page."
